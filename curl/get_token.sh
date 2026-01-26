cf36b0352975488368e757040428a479681f8259
Bob Harold <rharolde@umich.edu>
2026-01-26 13:35:22 -0500

    cleanup

--- curl/get_token.sh
+++ curl/get_token.sh
@@ -3,11 +3,7 @@
 # reads user and pw from environment variables
 x=$(curl $BLUECAT_CURL_OPTIONS -sS -X POST -H "Content-Type: application/json" -d '{"username": "'$BLUECAT_USERNAME'","password": "'$BLUECAT_PASSWORD'"}' https://$BLUECAT_SERVER/api/v2/sessions)
 BLUECAT_BASICAUTH=$(sed -E -e 's/^.*basicAuthenticationCredentials":"([^"]+)".*$/\1/' <<< "$x")
+echo "run this next line to add it to your environment:"
 echo "export BLUECAT_BASICAUTH=$BLUECAT_BASICAUTH"
 
-# how to log out at the end of a session:
-# x=$(curl -sS -X 'PATCH' 'https://'$BLUECAT_SERVER'/api/v2/sessions/current' -d '{"state": "LOGGED_OUT"}' -H 'Content-Type: application/merge-patch+json' -H 'Authorization: Basic '"$BLUECAT_BASICAUTH")
-# if [[ ! ( "$x" =~ LOGGED_OUT ) ]]; then
-#   echo "ERROR - failed to log out? $x"
-#   exit 2
-# fi
+# to end a session, see "logout.sh"
