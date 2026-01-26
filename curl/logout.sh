ab03d212a8809c01e75fbe8bd0a6ec2873819df3
Bob Harold <rharolde@umich.edu>
2026-01-26 13:34:45 -0500

    format better

--- curl/logout.sh
+++ curl/logout.sh
@@ -2,7 +2,10 @@
 # logout.sh
 # logs out the token created in get_token.sh
 
-x=$(curl -sS -X 'PATCH' 'https://'$BLUECAT_SERVER'/api/v2/sessions/current' -d '{"state": "LOGGED_OUT"}' -H 'Content-Type: application/merge-patch+json' -H 'Authorization: Basic '"$BLUECAT_BASICAUTH")
+x=$(curl -sS -X 'PATCH' 'https://'$BLUECAT_SERVER'/api/v2/sessions/current' 
+    -d '{"state": "LOGGED_OUT"}' 
+    -H 'Content-Type: application/merge-patch+json' 
+    -H 'Authorization: Basic '"$BLUECAT_BASICAUTH")
 if [[ ! ( "$x" =~ LOGGED_OUT ) ]]; then
   echo "ERROR - failed to log out? $x"
   exit 2
